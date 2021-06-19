6b4114b210b2226a69d634ce9567e1fc4bfd86f3
Yaren Zelal TOSU <yarentosu@gmail.com>
2021-06-19 04:00:34 +0300

    Update question4.c

--- question4.c
+++ question4.c
@@ -1,3 +1,7 @@
+/* QUESTION 4
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+Find the largest palindrome made from the product of two 3-digit numbers. */
+
 #include <stdio.h>
 
 int polindrom(unsigned int num);
